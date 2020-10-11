df4c96ace7b34fa7bf3f08a95acdfce40c06046d
Rodrigo Jara <rodrigojara1997@gmail.com>
2020-10-11 11:11:00 -0300

    Creado permisos para modulo de usuarios y config. Agregado al rol inicial admin sus permisos

--- app/__init__.py
+++ app/__init__.py
@@ -45,6 +45,8 @@ if not site:
     db.session.add(site_new)
     db.session.commit()
 
+# ----------------     Creación de datos inciales para la apliación             --------------------- 
+
 #Creacion del admin inicial del sistema si no existe
 user = db.session.query(User).filter_by(email='admin').first()
 if not user:
@@ -73,6 +75,99 @@ if not rol_operador:
 
     db.session.add(rol_operador)
     db.session.commit()
+
+#Crea rol operador si no existe
+rol_user = db.session.query(Rol).filter_by(name='user').first()
+if not rol_user:
+    rol_user = Rol(name='user')
+
+    db.session.add(rol_user)
+    db.session.commit()    
+
+#Crear permisos
+#Permisos para modulo config
+permiso_config_index = db.session.query(Permiso).filter_by(name='config_index').first()
+if not permiso_config_index:
+    permiso_config_index = Permiso(name='config_index')
+
+    db.session.add(permiso_config_index)
+    db.session.commit()
+
+permiso_config_new = db.session.query(Permiso).filter_by(name='config_new').first()
+if not permiso_config_new:
+    permiso_config_new = Permiso(name='config_new')
+
+    db.session.add(permiso_config_new)
+    db.session.commit()
+
+permiso_config_destroy = db.session.query(Permiso).filter_by(name='config_destroy').first()
+if not permiso_config_destroy:
+    permiso_config_destroy = Permiso(name='config_destroy')
+
+    db.session.add(permiso_config_destroy)
+    db.session.commit()
+
+permiso_config_update = db.session.query(Permiso).filter_by(name='config_update').first()
+if not permiso_config_update:
+    permiso_config_update = Permiso(name='config_update')
+
+    db.session.add(permiso_config_update)
+    db.session.commit()
+
+permiso_config_show = db.session.query(Permiso).filter_by(name='config_show').first()
+if not permiso_config_show:
+    permiso_config_show = Permiso(name='config_show')
+
+    db.session.add(permiso_config_show)
+    db.session.commit()
+
+#Permisos para modulo usuario
+permiso_user_index = db.session.query(Permiso).filter_by(name='user_index').first()
+if not permiso_user_index:
+    permiso_user_index = Permiso(name='user_index')
+
+    db.session.add(permiso_user_index)
+    db.session.commit()
+
+permiso_user_new = db.session.query(Permiso).filter_by(name='user_new').first()
+if not permiso_user_new:
+    permiso_user_new = Permiso(name='user_new')
+
+    db.session.add(permiso_user_new)
+    db.session.commit()
+
+permiso_user_destroy = db.session.query(Permiso).filter_by(name='user_destroy').first()
+if not permiso_user_destroy:
+    permiso_user_destroy = Permiso(name='user_destroy')
+
+    db.session.add(permiso_user_destroy)
+    db.session.commit()
+
+permiso_user_update = db.session.query(Permiso).filter_by(name='user_update').first()
+if not permiso_user_update:
+    permiso_user_update = Permiso(name='user_update')
+
+    db.session.add(permiso_user_update)
+    db.session.commit()
+
+permiso_user_show = db.session.query(Permiso).filter_by(name='user_show').first()
+if not permiso_user_show:
+    permiso_user_show = Permiso(name='user_show')
+
+    db.session.add(permiso_user_show)
+    db.session.commit()         
+
+#Relacionar permisos con rol
+rol_admin.permisos.append(permiso_config_index)
+rol_admin.permisos.append(permiso_config_new)
+rol_admin.permisos.append(permiso_config_destroy)
+rol_admin.permisos.append(permiso_config_update)
+rol_admin.permisos.append(permiso_config_show)
+rol_admin.permisos.append(permiso_user_index)
+rol_admin.permisos.append(permiso_user_new)
+rol_admin.permisos.append(permiso_user_destroy)
+rol_admin.permisos.append(permiso_user_update)
+rol_admin.permisos.append(permiso_user_show)
 
 #Relacionar al admin con el rol de admin
 user.roles.append(rol_admin)
