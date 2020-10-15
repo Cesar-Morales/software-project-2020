0763a52e79e4c52fc0824753aafc804a6a9dd196
Morales, Cesar Luis <moraleescesar@gmail.com>
2020-10-15 19:15:30 -0300

    prueba

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.resources import auth
 from app.resources import config
 from app.helpers import auth as helper_auth
 from flask_wtf import FlaskForm
-from app.static.forms import SearchForm
+from app.helpers.forms import SearchForm
 # Funciones que se exportan al contexto de Jinja2
 app.jinja_env.globals.update(is_authenticated=helper_auth.authenticated)
 
