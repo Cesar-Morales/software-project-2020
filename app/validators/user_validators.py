2a79245a7bd42ba5417c25e43e71572617717383
De Pian, Giuliano <yulomax@gmail.com>
2020-10-15 22:19:38 -0300

    fix 2 check_permission

--- app/validators/user_validators.py
+++ app/validators/user_validators.py
@@ -6,7 +6,7 @@ def check_permission(permiso):
     actual_roles = current_user.roles
     actual_permission = []
     for actual_rol in actual_roles:
-		for actual_permiso in actual_rol.permisos
-        actual_permission.append(actual_permiso)
+        for actual_permiso in actual_rol.permisos:
+            actual_permission.append(actual_permiso)
     return any(permisotemp.name
                == permiso for permisotemp in actual_permission)
