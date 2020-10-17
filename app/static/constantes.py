02aa52ba6fac50636b6a57c5ea3c88f8f7487913
Jara, Rodrigo Jose <rodrigojara1997@gmail.com>
2020-10-17 11:27:29 -0300

    Update constantes.py

--- app/static/constantes.py
+++ app/static/constantes.py
@@ -2,4 +2,4 @@ from app import db
 from app.models.site import Site
 
 #Seteamos variable para paginar registros
-ITEMS_PERPAGE = db.session.query(Site).first().pages
+ITEMS_PERPAGE = Site.obtain_site()
