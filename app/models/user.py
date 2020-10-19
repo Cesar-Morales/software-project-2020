2460c2b733f007df8705c9f0030e1a75a95d265a
GEMA, CARLOS SEBASTIáN <seebaa79@gmail.com>
2020-10-18 21:50:21 -0300

    a ver

--- app/models/user.py
+++ app/models/user.py
@@ -84,11 +84,7 @@ class User(db.Model, UserMixin):
         """Metodo que busca y retorna el usuario con email y password pasados como parametros"""
         return  db.session.query(User).filter_by(email=em,password=pas).first() 
 
-    
-
     def search(requestform):
-        """Funcion de busqueda de usuarios: se obtiene el string en el campo search, y si se busca por activo o bloqueado en el campo active:
-        si no esta seteado el username, solo se busca segun criterio de activado/desactivado. Caso contrario, se busca tanto por string como por estado"""
         usernam = requestform.get("search")
         actdeact = requestform.get("active")
         #Si no ingresan string a buscar, traigo todos los usuarios, verificando si mandaron activos o bloqueados.
