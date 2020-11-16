f124e173b71bef64ced58563a5cbce40fe8bf7e3
GEMA, CARLOS SEBASTIáN <seebaa79@gmail.com>
2020-11-15 22:21:38 -0300

    intentando con el map

--- app/static/map.js
+++ app/static/map.js
@@ -1,7 +1,24 @@
+
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-let myMap = L.map('mapid').setView([51.505,-0.09],13)
+let myMap = L.map('mapid').setView([-34.6083,-58.3712],13)
 
 L.tileLayer(tilesProvider,{
     maxZoom:18,
 }).addTo(myMap)
+
+var myIcon = L.icon({
+    iconUrl:"../static/marker.png",
+    iconSize:[50,32],
+    iconAnchor:[25,16],
+})
+
+
+ $('table tr td').click(function(){
+    var coordenadas = 
+    var marker = L.marker([coordenadas],{icon:myIcon})
+    .addTo(myMap)
+    myMap.setView([coordenadas],13)
+    console.log("PROBANDO");
+    return false;
+ });
